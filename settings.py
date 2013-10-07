c01379149ed595b1e6ac92eb59fc165def4a30f6
Matt Wolin <wolinm@gmail.com>
2013-10-07 12:46:24 -0500

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,7 @@
+DOWNLOAD_LOCATION = '/var/shutterrunner/images/'
+PHOTO_ENDPOINT = 'http://www.shutterrunner.com/posts/photoframe/'
+PHOTO_ENDPOINT_TEST = 'http://www.shutterrunner.com/posts/photoframe_test/'
+NUM_NEW_PHOTOS = '5'
+IMAGE_TIMEOUT = 5
+RESOLUTION_WIDTH = 1360
+RESOLUTION_HEIGHT = 768
